fd318eea627f680240fced7890f01e44a8b05709
Geoffroy Jaffa Planquart <geoffroy@planquart.fr>
2021-05-05 18:56:00 +0200

    remove _url helpers as non-available in rails 3

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -3,25 +3,9 @@ require "test_helper"
 class HelperTest < ActionView::TestCase
   tests Webpacker::Helper
 
-  attr_reader :request
-
-  def setup
-    @request = Class.new do
-      def send_early_hints(links) end
-      def base_url
-        "https://example.com"
-      end
-    end.new
-  end
-
   def test_asset_pack_path
     assert_equal "/packs/bootstrap-300631c4f0e0f9c865bc.js", asset_pack_path("bootstrap.js")
     assert_equal "/packs/bootstrap-c38deda30895059837cf.css", asset_pack_path("bootstrap.css")
-  end
-
-  def test_asset_pack_url
-    assert_equal "https://example.com/packs/bootstrap-300631c4f0e0f9c865bc.js", asset_pack_url("bootstrap.js")
-    assert_equal "https://example.com/packs/bootstrap-c38deda30895059837cf.css", asset_pack_url("bootstrap.css")
   end
 
   def test_image_pack_path
@@ -32,50 +16,42 @@ class HelperTest < ActionView::TestCase
     assert_equal "/packs/media/images/nested/image-c38deda30895059837cf.jpg", image_pack_path("media/images/nested/image.jpg")
   end
 
-  def test_image_pack_url
-    assert_equal "https://example.com/packs/application-k344a6d59eef8632c9d1.png", image_pack_url("application.png")
-    assert_equal "https://example.com/packs/media/images/image-c38deda30895059837cf.jpg", image_pack_url("image.jpg")
-    assert_equal "https://example.com/packs/media/images/image-c38deda30895059837cf.jpg", image_pack_url("media/images/image.jpg")
-    assert_equal "https://example.com/packs/media/images/nested/image-c38deda30895059837cf.jpg", image_pack_url("nested/image.jpg")
-    assert_equal "https://example.com/packs/media/images/nested/image-c38deda30895059837cf.jpg", image_pack_url("media/images/nested/image.jpg")
-  end
-
   def test_image_pack_tag
     assert_equal \
-      "<img alt=\"Edit Entry\" src=\"/packs/application-k344a6d59eef8632c9d1.png\" width=\"16\" height=\"10\" />",
+      "<img alt=\"Edit Entry\" height=\"10\" src=\"/packs/application-k344a6d59eef8632c9d1.png\" width=\"16\" />",
       image_pack_tag("application.png", size: "16x10", alt: "Edit Entry")
     assert_equal \
-      "<img alt=\"Edit Entry\" src=\"/packs/media/images/image-c38deda30895059837cf.jpg\" width=\"16\" height=\"10\" />",
+      "<img alt=\"Edit Entry\" height=\"10\" src=\"/packs/media/images/image-c38deda30895059837cf.jpg\" width=\"16\" />",
       image_pack_tag("image.jpg", size: "16x10", alt: "Edit Entry")
     assert_equal \
-      "<img alt=\"Edit Entry\" src=\"/packs/media/images/image-c38deda30895059837cf.jpg\" width=\"16\" height=\"10\" />",
+      "<img alt=\"Edit Entry\" height=\"10\" src=\"/packs/media/images/image-c38deda30895059837cf.jpg\" width=\"16\" />",
       image_pack_tag("media/images/image.jpg", size: "16x10", alt: "Edit Entry")
     assert_equal \
-      "<img alt=\"Edit Entry\" src=\"/packs/media/images/nested/image-c38deda30895059837cf.jpg\" width=\"16\" height=\"10\" />",
+      "<img alt=\"Edit Entry\" height=\"10\" src=\"/packs/media/images/nested/image-c38deda30895059837cf.jpg\" width=\"16\" />",
       image_pack_tag("nested/image.jpg", size: "16x10", alt: "Edit Entry")
     assert_equal \
-      "<img alt=\"Edit Entry\" src=\"/packs/media/images/nested/image-c38deda30895059837cf.jpg\" width=\"16\" height=\"10\" />",
+      "<img alt=\"Edit Entry\" height=\"10\" src=\"/packs/media/images/nested/image-c38deda30895059837cf.jpg\" width=\"16\" />",
       image_pack_tag("media/images/nested/image.jpg", size: "16x10", alt: "Edit Entry")
     assert_equal \
-      "<img srcset=\"/packs/media/images/image-2x-7cca48e6cae66ec07b8e.jpg 2x\" src=\"/packs/media/images/image-c38deda30895059837cf.jpg\" />",
+      "<img alt=\"Image-c38deda30895059837cf\" src=\"/packs/media/images/image-c38deda30895059837cf.jpg\" srcset=\"/packs/media/images/image-2x-7cca48e6cae66ec07b8e.jpg 2x\" />",
       image_pack_tag("media/images/image.jpg", srcset: { "media/images/image-2x.jpg" => "2x" })
   end
 
   def test_favicon_pack_tag
     assert_equal \
-      "<link rel=\"apple-touch-icon\" type=\"image/png\" href=\"/packs/application-k344a6d59eef8632c9d1.png\" />",
+      "<link href=\"/packs/application-k344a6d59eef8632c9d1.png\" rel=\"apple-touch-icon\" type=\"image/png\" />",
       favicon_pack_tag("application.png", rel: "apple-touch-icon", type: "image/png")
     assert_equal \
-      "<link rel=\"apple-touch-icon\" type=\"image/png\" href=\"/packs/media/images/mb-icon-c38deda30895059837cf.png\" />",
+      "<link href=\"/packs/media/images/mb-icon-c38deda30895059837cf.png\" rel=\"apple-touch-icon\" type=\"image/png\" />",
       favicon_pack_tag("mb-icon.png", rel: "apple-touch-icon", type: "image/png")
     assert_equal \
-      "<link rel=\"apple-touch-icon\" type=\"image/png\" href=\"/packs/media/images/mb-icon-c38deda30895059837cf.png\" />",
+      "<link href=\"/packs/media/images/mb-icon-c38deda30895059837cf.png\" rel=\"apple-touch-icon\" type=\"image/png\" />",
       favicon_pack_tag("media/images/mb-icon.png", rel: "apple-touch-icon", type: "image/png")
     assert_equal \
-      "<link rel=\"apple-touch-icon\" type=\"image/png\" href=\"/packs/media/images/nested/mb-icon-c38deda30895059837cf.png\" />",
+      "<link href=\"/packs/media/images/nested/mb-icon-c38deda30895059837cf.png\" rel=\"apple-touch-icon\" type=\"image/png\" />",
       favicon_pack_tag("nested/mb-icon.png", rel: "apple-touch-icon", type: "image/png")
     assert_equal \
-      "<link rel=\"apple-touch-icon\" type=\"image/png\" href=\"/packs/media/images/nested/mb-icon-c38deda30895059837cf.png\" />",
+      "<link href=\"/packs/media/images/nested/mb-icon-c38deda30895059837cf.png\" rel=\"apple-touch-icon\" type=\"image/png\" />",
       favicon_pack_tag("media/images/nested/mb-icon.png", rel: "apple-touch-icon", type: "image/png")
   end
 
@@ -97,49 +73,49 @@ class HelperTest < ActionView::TestCase
 
   def test_javascript_pack_tag
     assert_equal \
-      %(<script src="/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js"></script>\n) +
-        %(<script src="/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js"></script>\n) +
-        %(<script src="/packs/application-k344a6d59eef8632c9d1.js"></script>\n) +
-        %(<script src="/packs/bootstrap-300631c4f0e0f9c865bc.js"></script>),
+      %(<script src="/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js" type="text/javascript"></script>\n) +
+        %(<script src="/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js" type="text/javascript"></script>\n) +
+        %(<script src="/packs/application-k344a6d59eef8632c9d1.js" type="text/javascript"></script>\n) +
+        %(<script src="/packs/bootstrap-300631c4f0e0f9c865bc.js" type="text/javascript"></script>),
       javascript_pack_tag("application", "bootstrap")
   end
 
   def test_javascript_pack_tag_splat
     assert_equal \
-      %(<script src="/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js" defer="defer"></script>\n) +
-        %(<script src="/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js" defer="defer"></script>\n) +
-        %(<script src="/packs/application-k344a6d59eef8632c9d1.js" defer="defer"></script>),
+      %(<script defer="defer" src="/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js" type="text/javascript"></script>\n) +
+        %(<script defer="defer" src="/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js" type="text/javascript"></script>\n) +
+        %(<script defer="defer" src="/packs/application-k344a6d59eef8632c9d1.js" type="text/javascript"></script>),
       javascript_pack_tag("application", defer: true)
   end
 
   def test_javascript_pack_tag_symbol
     assert_equal \
-      %(<script src="/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js"></script>\n) +
-        %(<script src="/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js"></script>\n) +
-        %(<script src="/packs/application-k344a6d59eef8632c9d1.js"></script>),
+      %(<script src="/packs/vendors~application~bootstrap-c20632e7baf2c81200d3.chunk.js" type="text/javascript"></script>\n) +
+        %(<script src="/packs/vendors~application-e55f2aae30c07fb6d82a.chunk.js" type="text/javascript"></script>\n) +
+        %(<script src="/packs/application-k344a6d59eef8632c9d1.js" type="text/javascript"></script>),
       javascript_pack_tag(:application)
   end
 
   def test_stylesheet_pack_tag
     assert_equal \
-      %(<link rel="stylesheet" media="screen" href="/packs/1-c20632e7baf2c81200d3.chunk.css" />\n) +
-        %(<link rel="stylesheet" media="screen" href="/packs/application-k344a6d59eef8632c9d1.chunk.css" />\n) +
-        %(<link rel="stylesheet" media="screen" href="/packs/hello_stimulus-k344a6d59eef8632c9d1.chunk.css" />),
+      %(<link href="/packs/1-c20632e7baf2c81200d3.chunk.css" media="screen" rel="stylesheet" type="text/css" />\n) +
+        %(<link href="/packs/application-k344a6d59eef8632c9d1.chunk.css" media="screen" rel="stylesheet" type="text/css" />\n) +
+        %(<link href="/packs/hello_stimulus-k344a6d59eef8632c9d1.chunk.css" media="screen" rel="stylesheet" type="text/css" />),
       stylesheet_pack_tag("application", "hello_stimulus")
   end
 
   def test_stylesheet_pack_tag_symbol
     assert_equal \
-      %(<link rel="stylesheet" media="screen" href="/packs/1-c20632e7baf2c81200d3.chunk.css" />\n) +
-        %(<link rel="stylesheet" media="screen" href="/packs/application-k344a6d59eef8632c9d1.chunk.css" />\n) +
-        %(<link rel="stylesheet" media="screen" href="/packs/hello_stimulus-k344a6d59eef8632c9d1.chunk.css" />),
+      %(<link href="/packs/1-c20632e7baf2c81200d3.chunk.css" media="screen" rel="stylesheet" type="text/css" />\n) +
+        %(<link href="/packs/application-k344a6d59eef8632c9d1.chunk.css" media="screen" rel="stylesheet" type="text/css" />\n) +
+        %(<link href="/packs/hello_stimulus-k344a6d59eef8632c9d1.chunk.css" media="screen" rel="stylesheet" type="text/css" />),
       stylesheet_pack_tag(:application, :hello_stimulus)
   end
 
   def test_stylesheet_pack_tag_splat
     assert_equal \
-      %(<link rel="stylesheet" media="all" href="/packs/1-c20632e7baf2c81200d3.chunk.css" />\n) +
-        %(<link rel="stylesheet" media="all" href="/packs/application-k344a6d59eef8632c9d1.chunk.css" />),
+      %(<link href="/packs/1-c20632e7baf2c81200d3.chunk.css" media="all" rel="stylesheet" type="text/css" />\n) +
+        %(<link href="/packs/application-k344a6d59eef8632c9d1.chunk.css" media="all" rel="stylesheet" type="text/css" />),
       stylesheet_pack_tag("application", media: "all")
   end
 end
